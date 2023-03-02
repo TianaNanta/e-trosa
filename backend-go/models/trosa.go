daf378e588e5917c4526ab6daa03b011d67012c2
TianaNanta <andrianjakananta@gmail.com>
2023-03-02 10:09:23 +0300

    modify trosa model

--- backend-go/models/trosa.go
+++ backend-go/models/trosa.go
@@ -6,10 +6,9 @@ import (
 
 type Trosa struct {
 	gorm.Model
-	Amount      int    `json:"amount"`
-	Note        string `json:"note"`
-	OwnerRefer  int    `json:"user_id"`
-	Owner       User   `json:"owner" gorm:"foreignKey:OwnerRefer"`
-	InDeptRefer int    `json:"in_dept_id"`
-	InDept      User   `json:"in_dept" gorm:"foreignKey:InDeptRefer"`
+	Amount     int    `json:"amount"`
+	Note       string `json:"note"`
+	OwnerRefer int    `json:"user_id"`
+	Owner      User   `json:"owner" gorm:"foreignKey:OwnerRefer"`
+	InDept     int    `json:"in_dept"`
 }
